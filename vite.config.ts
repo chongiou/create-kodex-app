57684c13f6f4c4082467824c11fcf1452c2f5ee7
重楼 <chongiou@qq.com>
2025-07-16 13:42:15 +0800

    修改构建配置，以 cli 修复找不对文件的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,30 @@
 import { defineConfig } from 'vite'
 import { dependencies } from './package.json'
+import fs from 'fs-extra'
+
+function copyTemplates() {
+  return {
+    name: 'copy-templates',
+    closeBundle() {
+      fs.copySync('templates', 'dist/templates')
+      console.log('✅ Templates copied!')
+    }
+  }
+}
 
 export default defineConfig({
+  plugins: [
+    copyTemplates()
+  ],
   build: {
-    target: 'esnext',
+    target: 'node20',
     minify: false,
     outDir: 'dist',
     lib: {
-      entry: 'bin/index.ts',
+      entry: {
+        'bin/index': 'bin/index.ts'
+      },
       formats: ['es'],
-      fileName: 'index',
     },
     rollupOptions: {
       external: [/^node:/, ...Object.keys(dependencies)],
